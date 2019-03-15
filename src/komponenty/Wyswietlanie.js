9adf87e5f9128542943525f7eac119ddd031194c
rafał-Komputer\rafał <rafallukasik123@wp.pl>
2019-03-15 12:51:24 +0100

    dodany sass

--- src/komponenty/Wyswietlanie.js
+++ src/komponenty/Wyswietlanie.js
@@ -13,14 +13,14 @@ constructor(props){
 
   createTask(item){
 
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
+    let today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); 
+    const yyyy = today.getFullYear();
     
     today = mm + ' ' + dd + ' ' + yyyy;
     
-    console.log(today);
+    
     return  <li key={item.key} >
     {item.tekst} 
     {today}
