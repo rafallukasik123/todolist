f4e5d107205bdc1d9e9d01eecdd27bf63ab0455a
rafał-Komputer\rafał <rafallukasik123@wp.pl>
2019-03-15 20:25:19 +0100

    Updates

---
+++ precache-manifest.66e9866c771967813fbe42316c6c0ad8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  },
+  {
+    "revision": "a50cc55ae9355408decc",
+    "url": "/todolist/static/js/main.26f278cc.chunk.js"
+  },
+  {
+    "revision": "65998e6468081ae26981",
+    "url": "/todolist/static/js/2.5b26626a.chunk.js"
+  },
+  {
+    "revision": "a50cc55ae9355408decc",
+    "url": "/todolist/static/css/main.63321d6f.chunk.css"
+  },
+  {
+    "revision": "2a60145e272f86ae3d137cda7daf1d6c",
+    "url": "/todolist/index.html"
+  }
+];
